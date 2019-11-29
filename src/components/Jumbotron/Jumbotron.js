40ccf6278eac5f65a57739e7c5d708d955863dc1
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-29 11:55:04 +0000

    CSS

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Jumbotron.css';
 
 const Jumbotron = (props) => (
-<div className="jumbotron">
-  <h1>Xmas Wilders Memory Game!</h1>
-  <p>Pick a new Wilder picture everytime.</p>
-  <p>Score: {props.score} <span>/ Highest score: {props.highScore} </span></p>
-</div>
+  <div className="jumbotron">
+    <h1>Xmas Wilders Memory Game!</h1>
+    <p className='title'>Pick a new Wilder picture everytime.</p>
+    <p className='score'>Score: {props.score} <span>/ Highest score: {props.highScore} </span></p>
+  </div>
 )
 
 export default Jumbotron;
